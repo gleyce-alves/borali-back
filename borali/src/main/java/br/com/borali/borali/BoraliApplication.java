597d2d10290f363538a9e68336b5b205b4361ad9
AndreGomes <67878678+AndreLuisGomes@users.noreply.github.com>
2022-08-08 20:32:46 -0300

    novo projeto

---
+++ borali/src/main/java/br/com/borali/borali/BoraliApplication.java
@@ -0,0 +1,13 @@
+package br.com.borali.borali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraliApplication.class, args);
+	}
+
+}
