c86e2ee6db12e9f0811c2af616047bc5d04ba1f9
AndreGomes <67878678+AndreLuisGomes@users.noreply.github.com>
2022-08-09 13:55:06 -0300

    novo projeto com xampp

---
+++ borali-back-1/src/main/java/com/borali/Controller.java
@@ -0,0 +1,16 @@
+package com.borali;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	// AQUI REDIRECIONAMOS TODAS AS TELAS
+	
+	// PAGINAS HOME
+	
+	@GetMapping("/")
+	public String home() {
+		return "home-buscar";
+	}
+}
