06ac71791d21bbc0b04c29c67579e6c0f77d01b4
AndreGomes <andreluisgomes09@gmail.com>
2022-08-09 16:24:17 -0300

    adicionado funções e realocado o controller

---
+++ borali-back-1/src/main/java/com/borali/application/BoraliBack1Application.java
@@ -0,0 +1,13 @@
+package com.borali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraliBack1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraliBack1Application.class, args);
+	}
+
+}
