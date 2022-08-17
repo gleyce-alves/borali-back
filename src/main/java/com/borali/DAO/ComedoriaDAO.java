dce0f0ad35f85dddea067dfc2341e3a32a972e18
AndreGomes <67878678+AndreLuisGomes@users.noreply.github.com>
2022-08-17 00:57:07 -0300

    Delete src directory

--- src/main/java/com/borali/DAO/ComedoriaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.borali.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.borali.entity.Comedoria;
-
-public interface ComedoriaDAO extends JpaRepository<Comedoria, Integer>{
-
-}
